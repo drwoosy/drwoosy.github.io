009752c082f1458b25506d97fc1ca651a3ae526b
misochasm <159483945+misochasm@users.noreply.github.com>
2024-09-24 13:43:15 -0700

    new project
    woah

---
+++ script.js
@@ -0,0 +1,50 @@
+gsap.registerPlugin(ScrollTrigger);
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = [
+    { id: "#card-1", endTranslateX: -2000, rotate: 45 },
+    { id: "#card-2", endTranslateX: -1000, rotate: -30 },
+    { id: "#card-3", endTranslateX: -2000, rotate: 45 },
+    { id: "#card-4", endTranslateX: -1500, rotate: -30 },
+  ];
+
+  ScrollTrigger.create({
+    trigger: ".wrapper-404",
+    start: "top top",
+    end: "+=900vh",
+    scrub: 1,
+	pin: true,
+	pinSpacing: true,
+    onUpdate: (self) => {
+      gsap.to(".wrapper-404", {
+        x: `${-350 * self.progress}vw`,
+        duration: 0.5,
+        ease: "power3.out",
+		y: `${-20 * self.progress}vh`,
+      });
+    },
+  });
+
+  cards.forEach((card) => {
+    ScrollTrigger.create({
+      trigger: "card.id",
+      start: "top top",
+      end: "+=1200vh",
+      scrub: 1,
+      onUpdate: (self) => {
+        gsap.to(card.id, {
+          x: `${card.endTranslateX * self.progress}px`,
+          rotate: `${card.rotate * self.progress * 2}`,
+          duration: 0.5,
+          ease: "power3.out",
+        });
+      },
+    });
+  });
+});
+
+window.onscroll = function() {
+  var progressBar = document.getElementById('progress-bar');
+  var scrollTotal = document.documentElement.scrollHeight - window.innerHeight;
+  var scrollPercent = (window.scrollY / scrollTotal) * 100;
+  progressBar.style.width = scrollPercent + "%";
+};
